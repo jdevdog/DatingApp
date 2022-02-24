9fe7c1bda0619c6950503ee9032e6775dac0bf68
Josh George <joshgeorge2016@gmail.com>
2022-02-24 16:15:21 -0500

    end of s. 8

---
+++ API/Data/Seed.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<AppUser>>(userData);
+            foreach(var user in users)
+            {
+                using var hmac = new HMACSHA512();
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
