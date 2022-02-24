905b4e0fdb93bc7cb621ffb62985f83057987b27
Josh George <joshgeorge2016@gmail.com>
2022-02-24 09:24:35 -0500

    end of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto 
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
